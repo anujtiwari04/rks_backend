f0659b476d4c3f38ab616f1ccc9d63bfdf5138d3
Anuj Tiwari <anujtiwari31135@gmail.com>
2025-11-05 11:32:18 +0530

    signup done

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -3,9 +3,9 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/user.model');
 
 exports.signup = async (req, res) => {
-  const { email, password } = req.body;
-  if (!email || !password)
-    return res.status(400).json({ message: 'Email and password required' });
+  const { email, password, name } = req.body;
+  if (!email || !password || !name)
+    return res.status(400).json({ message: 'Email, password and name required' });
 
   try {
     const exists = await User.findOne({ email });
@@ -13,20 +13,26 @@ exports.signup = async (req, res) => {
       return res.status(409).json({ message: 'Email already registered' });
 
     const hashed = await bcrypt.hash(password, 10);
-    const user = await User.create({ email, password: hashed });
+    const user = await User.create({
+      name,              
+      email,
+      password: hashed,
+    });
+
 
     const token = jwt.sign(
-  { id: user._id, email: user.email },
-  process.env.JWT_SECRET,
-  { expiresIn: '7d' }
-);
+      { id: user._id, email: user.email },
+      process.env.JWT_SECRET,
+      { expiresIn: '7d' }
+    );
 
-res.status(201).json({
-  id: user._id,
-  email: user.email,
-  token,
-  message: 'Signup successful'
-});
+    res.status(201).json({
+      id: user._id,
+      email: user.email,
+      name: user.name,
+      token,
+      message: 'Signup successful'
+    });
 
 
   } catch (err) {
@@ -43,7 +49,7 @@ exports.login = async (req, res) => {
   try {
     const user = await User.findOne({ email });
     if (!user)
-      return res.status(401).json({ message: 'No email found' }); 
+      return res.status(401).json({ message: 'No email found' });
 
     const match = await bcrypt.compare(password, user.password);
     if (!match)
@@ -58,6 +64,7 @@ exports.login = async (req, res) => {
     res.json({
       id: user._id,
       email: user.email,
+      name: user.name,
       token
     });
   } catch (err) {
